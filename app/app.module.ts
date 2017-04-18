2a5f78446f8079601f93560863fdfbc97ed91e8a
rbarcelos <rbarcelos@gmail.com>
2017-04-18 14:35:02 -0700

    removing footer

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,7 +8,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { DashboardModule } from './dashboard/dashboard.module';
 import { SidebarModule } from './sidebar/sidebar.module';
-import { FooterModule } from './shared/footer/footer.module';
 import { NavbarModule} from './shared/navbar/navbar.module';
 
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
@@ -19,7 +18,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
         DashboardModule,
         SidebarModule,
         NavbarModule,
-        FooterModule,
         RouterModule.forRoot([])
     ],
     declarations: [ AppComponent, DashboardComponent ],
