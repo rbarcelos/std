50d238860ce4699a6e877af22b15181c7cde1490
rbarcelos <rbarcelos@gmail.com>
2017-04-20 23:57:54 -0700

    seting pincipal color

--- app/shared/FloatingButton/floatingButton.component.ts
+++ app/shared/FloatingButton/floatingButton.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'floating-cmp',
+//    template:'<div style="position:fixed; left:200px;z-index:100" (click)="onClick()">    <button>Click</button>    </div>'
     templateUrl: 'floatingButton.component.html',
     styleUrls:['../../../assets/css/typography.css','../../../assets/css/google-icons.css','../../../assets/css/button.css','../../../assets/css/floating-action-button.css']
 })
@@ -10,5 +11,9 @@ import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 export class FloatingButtonComponent implements OnInit{
 
     ngOnInit(){
+    }
+    
+    onClick() {
+        alert('Click');
     }
 }
