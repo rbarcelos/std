872202aeddfc42ac59846a0db57d5e501cb5f395
Rafael Barcelos <rbarcelo@microsoft.com>
2017-04-21 11:43:47 -0700

    UX improvements

--- app/sidebar/sidebar-routes.config.js
+++ app/sidebar/sidebar-routes.config.js
@@ -2,8 +2,9 @@
 exports.ROUTES = [
     { path: 'rotas', title: 'Rotas', icon: 'map', class: '' },
     { path: 'notas', title: 'Notas Fiscais', icon: 'receipt', class: '' },
+    { path: 'users', title: 'Emissores', icon: 'group', class: '' },
+    { path: 'destinos', title: 'Destinos de entrega', icon: 'beenhere', class: '' },
     { path: 'cadastro', title: 'Cadastro', icon: 'my_library_add', class: '' },
-    { path: 'settings', title: 'Configurações', icon: 'settings', class: '' },
     { path: 'dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
     { path: 'icons', title: 'Icons', icon: 'bubble_chart', class: '' },
 ];
--- app/sidebar/sidebar-routes.config.ts
+++ app/sidebar/sidebar-routes.config.ts
@@ -4,10 +4,11 @@ export const ROUTES: RouteInfo[] = [
 
     { path: 'rotas', title: 'Rotas',  icon:'map', class: '' },
     { path: 'notas', title: 'Notas Fiscais',  icon:'receipt', class: '' },
+    { path: 'users', title: 'Emissores',  icon:'group', class: '' },
+    { path: 'destinos', title: 'Destinos de entrega',  icon:'beenhere', class: '' },
     { path: 'cadastro', title: 'Cadastro',  icon:'my_library_add', class: '' },
-    { path: 'settings', title: 'Configurações',  icon:'settings', class: '' },
+
     { path: 'dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    
     { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
   
     
