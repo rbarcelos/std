dc7c5bea01f5f521c71d67230c6d08829de1c7f5
rbarcelos <rbarcelos@gmail.com>
2017-04-22 23:31:40 -0700

    updating few minor issues

--- app/sidebar/sidebar-routes.config.js
+++ app/sidebar/sidebar-routes.config.js
@@ -1,11 +1,46 @@
 "use strict";
 exports.ROUTES = [
-    { path: 'rotas', title: 'Rotas', icon: 'map', class: '' },
-    { path: 'notas', title: 'Notas Fiscais', icon: 'receipt', class: '' },
-    { path: 'users', title: 'Emissores', icon: 'group', class: '' },
-    { path: 'destinos', title: 'Destinos de entrega', icon: 'beenhere', class: '' },
-    { path: 'cadastro', title: 'Cadastro', icon: 'apps', class: '' },
-    { path: 'dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
-    { path: 'icons', title: 'Icons', icon: 'bubble_chart', class: '' },
+    {
+        path: 'rotas',
+        title: 'Rotas',
+        icon: 'map',
+        class: ''
+    },
+    {
+        path: 'notas',
+        title: 'Notas Fiscais',
+        icon: 'receipt',
+        class: ''
+    },
+    {
+        path: 'users',
+        title: 'Emissores',
+        icon: 'group',
+        class: ''
+    },
+    {
+        path: 'destinos',
+        title: 'Destinos de entrega',
+        icon: 'beenhere',
+        class: ''
+    },
+    {
+        path: 'cadastro',
+        title: 'Cadastro',
+        icon: 'add_circle',
+        class: ''
+    },
+    {
+        path: 'dashboard',
+        title: 'Dashboard',
+        icon: 'dashboard',
+        class: ''
+    },
+    {
+        path: 'icons',
+        title: 'Icons',
+        icon: 'bubble_chart',
+        class: ''
+    },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
