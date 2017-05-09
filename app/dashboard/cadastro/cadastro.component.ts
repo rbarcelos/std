31f9dd7ad01d32ddf0a4375fd12495c06c3ce78a
rbarcelos <rbarcelos@gmail.com>
2017-05-09 00:43:05 -0700

    starting fresh

--- app/dashboard/cadastro/cadastro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'cadastro-cmp',
-    moduleId: module.id,
-    templateUrl: 'cadastro.component.html'
-})
-
-export class CadastroComponent{}
