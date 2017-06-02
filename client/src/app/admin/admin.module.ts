68ae174789cbca0317f90c8e3f5424573d2bc9eb
rbarcelos <rbarcelos@gmail.com>
2017-06-02 01:43:58 -0700

    Fixing material layout and adding cadastro component

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -5,15 +5,24 @@ import { AdminComponent } from './admin.component';
 import { AdminGuard } from "./admin.guard";
 import { ADMIN_ROUTES, ADMIN_COMPONENTS } from "./admin.routes";
 import { StdCommonModule } from "../common/common.module";
+import { CadastroComponent, DialogContent } from './distribuidoras/cadastro/cadastro.component';
+
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdInputModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     StdCommonModule,
+    MaterialModule,
+    MdInputModule,
+    BrowserAnimationsModule,
     RouterModule.forChild(ADMIN_ROUTES)
   ],
-  declarations: [ADMIN_COMPONENTS],
+  declarations: [ADMIN_COMPONENTS, CadastroComponent, DialogContent],
   exports: [AdminComponent],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, AdminGuard],
+  entryComponents: [DialogContent]
 })
 export class AdminModule { }
