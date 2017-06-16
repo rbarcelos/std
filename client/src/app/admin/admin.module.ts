8399cc6c2d6c4d3e5e8fc0a8d36f2c0e7a94e063
rbarcelos <rbarcelos@gmail.com>
2017-06-15 23:42:46 -0700

    cadastro

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -5,7 +5,7 @@ import { AdminComponent } from './admin.component';
 import { AdminGuard } from "./admin.guard";
 import { ADMIN_ROUTES, ADMIN_COMPONENTS } from "./admin.routes";
 import { StdCommonModule } from "../common/common.module";
-import { CadastroComponent, DialogContent } from './distribuidoras/cadastro/cadastro.component';
+import { CadastroComponent } from './distribuidoras/cadastro/cadastro.component';
 
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,11 +18,12 @@ import { MdInputModule } from '@angular/material';
     MaterialModule,
     MdInputModule,
     BrowserAnimationsModule,
-    RouterModule.forChild(ADMIN_ROUTES)
+    RouterModule.forChild(ADMIN_ROUTES),
+    NgxDatatableModule 
   ],
-  declarations: [ADMIN_COMPONENTS, CadastroComponent, DialogContent],
+  declarations: [ADMIN_COMPONENTS, CadastroComponent],
   exports: [AdminComponent],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }, AdminGuard],
-  entryComponents: [DialogContent]
+  entryComponents: [CadastroComponent]
 })
 export class AdminModule { }
--- client/src/app/admin/distribuidoras/distribuidoras.component.ts
+++ client/src/app/admin/distribuidoras/distribuidoras.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { CadastroComponent } from "./cadastro/cadastro.component";
+
+import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-distribuidoras',
   templateUrl: './distribuidoras.component.html',
   styleUrls: ['./distribuidoras.component.css']
 })
-export class DistribuidorasComponent implements OnInit {
+export class DistribuidorasComponent {
+  lastDialogResult: string;
 
-  constructor() { }
+  constructor(private dialog: MdDialog, private dataService: DataService) {
+ this.dat((data) => {
+      this.selected = [data[2]];
+      this.rows = data;
+    });
+  }
 
-  ngOnInit() {
+  openDialog() {
+    let dialogRef = this.dialog.open(CadastroComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.lastDialogResult = result;
+    })
+  }
+
+   rows = [];
+
+  selected = [];
+
+  columns: any[] = [
+    { prop: 'name'} , 
+    { name: 'Company' }, 
+    { name: 'Gender' }
+  ];
+
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/company.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  onSelect({ selected }) {
+    console.log('Select Event', selected, this.selected);
+  }
+
+  onActivate(event) {
+    console.log('Activate Event', event);
+  }
+
+  updateRowPosition() {
+    const ix = this.getSelectedIx();
+    const arr = [ ...this.rows ];
+    arr[ix - 1] = this.rows[ix];
+    arr[ix] = this.rows[ix - 1];
+    this.rows = arr;
+  }
+
+  getSelectedIx() {
+    return this.selected[0]['$$index'];
   }
 
 }
